020cb31ea348e7dddc33195fb8e0c69e9c0c2e7f
Thomas 'NoZip' Pizon <thomas.pizon@etu.u-bordeaux.fr>
2014-03-06 18:30:55 +0100

    Ajout de la gestion du type TrackPool. Hack-style

---
+++ include/TrackPool.h
@@ -0,0 +1,33 @@
+#ifndef TRACKPOOL_H
+#define TRACKPOOL_H
+
+#include <unordered_set>
+#include <functionnal>
+
+#include <Track>
+
+/*
+  Fichier de test, je suis absolument pas sûr que ça marche.
+ */
+
+/**
+ * Spécialisation de template pour pouvoir créer un set de pistes.
+ * @see http://www.cplusplus.com/reference/unordered_set/unordered_set/
+ */
+template <>
+struct std::hash<SoundCity::Track> {
+  std::size_t operator(const SoundCity::Track &track) {
+    return std::hash<int>()(track.getId());
+  }
+};
+
+namespace Soundcity {
+  /**
+   * Définition de Trackpool.
+   */
+  using TrackPool = std::set<Track>;
+}
+
+
+
+#endif
