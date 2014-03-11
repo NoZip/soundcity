e7550a2b63ba77d69151efbbbcd42369124f55e8
Baptiste Fumaroli <baptiste.fumaroli@gmail.com>
2014-03-11 16:57:34 -0700

    Mise a jour du test pour utiliser un Track complet, le test ne passe pas, à fixer

--- test/test_track.cpp
+++ test/test_track.cpp
@@ -1,4 +1,4 @@
-#include <../include/Track.h>
+#include <Track.h>
 #include <iostream>
 #include <string>
 
@@ -6,7 +6,27 @@ using namespace SoundCity;
 
 int main()
 {
-  std::string s("Test");
-  Artist a(1,s,0.4,1.3);
-  std::cout << a.getName() << "\n";
+  std::string name, albumName, name2;
+  name = "Test";
+  Artist art(1,name,0.4,1.0);
+  //std::cout << art.getName() << "\n";
+  albumName = "Testalbum";
+  name2 = "Test";
+  Album alb(1,albumName,name2,2012);
+  ContextData data(art,alb,0.5);
+  //std::cout << data.getArtistName() << "\n";
+  SignalData sign(0.2,0.3,0.4);
+  Track test(1,sign,data);
+  std::cout << "Track id : " << test.getId() << "\n";
+  std::cout << "Artist id : " << test.getArtistId() << "\n";
+  std::cout << "Artist name : " << test.getArtistName() << "\n";
+  std::cout << "Artist familiarity : " << test.getArtistFamiliarity() << "\n";
+  std::cout << "Artist popularity : " << test.getArtistPopularity() << "\n";
+  std::cout << "Album Title : " << test.getAlbumTitle() << "\n";
+  std::cout << "Album Artist : " << test.getAlbumArtist() << "\n";
+  std::cout << "Album Release : " << test.getAlbumReleaseDate() << "\n";
+  std::cout << "Track Popularity : " << test.getPopularity() << "\n";
+  std::cout << "Track Rhythm : " << test.getRhythm() << "\n";
+  std::cout << "Track Energy : " << test.getEnergy() << "\n";
+  std::cout << "Track Tonality : " << test.getTonality() << "\n";
 }
