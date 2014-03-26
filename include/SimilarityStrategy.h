5f9c68828adff720d7ad133fea811c68faba0933
Jean P <jean.pecqueur@gmail.com>
2014-03-27 00:44:56 +0100

    Ajout d'un header pour la stratégie de similarité

---
+++ include/SimilarityStrategy.h
@@ -0,0 +1,32 @@
+#ifndef SIMILARITYSTRATEGY_H
+#define SIMILARITYSTRATEGY_H 
+
+#include <Track.h>
+#include <ISimilarityStrategy.h>
+
+namespace SoundCity
+{
+
+  /**
+  * Header pour la gestion d'une similarité naïve 
+  * entre les morceaux basée sur un calcul simple
+  * sur le rythme
+  */
+
+  class SimilarityStrategy : public ISimilarityStrategy {
+  public: 
+
+      /**
+      * Permet de donner un score de similarité entre les
+      * deux morceaux passés en paramètre à partir de leur
+      * rythme.
+      * @param track1 Premier morceaux à comparer
+      * @param track2 Deuxième morceaux à comparer
+      */
+      float compute(Track track1, Track track2);
+      
+      };
+
+} //end of namespace Soundcity
+
+#endif
