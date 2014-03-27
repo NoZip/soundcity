dbc230819d91036f24de6cd97cae1b48c0d08fc7
Lucas V <lucasvauzelle@msn.com>
2014-03-27 09:30:25 +0100

    Implémentation de TextOutput.cpp (test à faire)

---
+++ src/TextOutput.cpp
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <cassert>
+#include <string>
+
+#include <TextOutput.h>
+
+void TextOutput::format(Playlist &playlist)
+{
+  file.open("resultat.txt", std::ofstream::out | std::ofstream::app);
+  assert(file.is_open());
+
+  for(Playlist::iterator i = Playlist.begin(); i != Playlist.end(); i++)
+  {
+  	file << *i.getTitle() << " - " << *i.getContextData().getArtist().getName()
+  		<< "(" << *i.getContextData().getAlbum().getReleaseDate() << ")" << endl;
+	}
+	file.close();
+}
