162ad62f8c4a87efbc6391d96c9a213e4dba3c8c
Baptiste Fumaroli <baptiste.fumaroli@gmail.com>
2014-03-31 14:56:45 +0200

    La stratégie utilise maintenant une fonction logarithmique

--- src/NaiveSimilarityStrategy.cpp
+++ src/NaiveSimilarityStrategy.cpp
@@ -6,26 +6,23 @@
 using namespace SoundCity;
 using namespace std;
 
-const int ALBUM_RELEASE_DATE_THRESHOLD = 10;
-const float DALBUM_RELEASE_DATE_THRESHOLD = 0.8;
+const float ALBUM_RELEASE_DATE_THRESHOLD = 0.3;
 
 NaiveSimilarityStrategy::NaiveSimilarityStrategy() {}
 
 float NaiveSimilarityStrategy::compute(Track track1, Track track2) const
 {
-  int date1 = track1.getAlbumReleaseDate();
-  int date2 = track2.getAlbumReleaseDate();
-  int dAlbumReleaseDate = abs(date1 - date2);
-
-  if(dAlbumReleaseDate == 0)
+  float date1 = track1.getAlbumReleaseDate();
+  float date2 = track2.getAlbumReleaseDate();
+  float dAlbumReleaseDate = log10(abs(date1 - date2));
+  if(dAlbumReleaseDate > 1)
   {
-    return 1;
+    dAlbumReleaseDate = 1;
   }
-  if(dAlbumReleaseDate <= ALBUM_RELEASE_DATE_THRESHOLD)
+  if(dAlbumReleaseDate < ALBUM_RELEASE_DATE_THRESHOLD)
   {
-    return DALBUM_RELEASE_DATE_THRESHOLD;
+    dAlbumReleaseDate = 0;
   }
-
-  return 0;
+  return 1 - dAlbumReleaseDate;
 }
 
