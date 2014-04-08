487efe619268f5ef2cf11d4d9580ca5bcd8ed3cf
Baptiste Fumaroli <baptiste.fumaroli@gmail.com>
2014-04-08 12:19:38 +0200

    Ajout de la différence dans le test

--- test/test_playlist_similarity.cpp
+++ test/test_playlist_similarity.cpp
@@ -4,7 +4,7 @@
 #include <SimilarityStrategy.h>
 #include <Generator.h>
 
-#define PARAMS 1
+#define PARAMS 2
 
 using namespace SoundCity;
 using std::cout;
@@ -13,7 +13,7 @@ using std::endl;
 void 
 usage (char *s)
 {
-  fprintf(stderr, "Usage: %s <database>\n", s);
+  fprintf(stderr, "Usage: %s <database> <k> \n", s);
   exit(EXIT_FAILURE);
 }
 
@@ -22,7 +22,7 @@ main(int argc, char *argv[])
 {
   if (argc != PARAMS+1)
     usage(argv[0]);
-
+  int k = atoi(argv[2]);
   SimilarityStrategy similarity;
   SQLiteDatabase database(argv[1]);
   OptionList options("",1,3000,0,0,0,0,25);
@@ -35,15 +35,25 @@ main(int argc, char *argv[])
   }
   Playlist playlist = generator.generate(options);
   int i = 1;
-  for(auto it = playlist.begin(); it != playlist.end();)
+  auto it = playlist.begin();
+  for(int n = 0; n < k-1; n++)
+  {
+    ++i; ++it;
+  }
+  while(it != playlist.end())
   {
     ++it; ++i;
     auto next = it; ++next;
-    auto previous = it; --previous;
+    auto previous = it;
+    for(int n = 0; n < k; n++)
+    {
+      --previous;
+    }
     if(next != playlist.end() && it != playlist.end())
     {
       cout << "Similarité entre le le morceau " << i <<  " et le morceau " << i+1 << " : " << similarity.compute(*it,*next) << endl;
-      cout << "Similarité entre le le morceau " << i-1 <<  " et le morceau " << i+1 << " : " << similarity.compute(*previous,*next) << endl;
+      cout << "Similarité entre le le morceau " << i-k <<  " et le morceau " << i+1 << " : " << similarity.compute(*previous,*next) << endl;
+      cout << "Différence " << i << "-" << i+1 << " | " << i-k << "-" << i+1 << " : "<< similarity.compute(*it,*next)-similarity.compute(*previous,*next) << endl;
     }
   }
 
